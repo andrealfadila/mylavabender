4900ea5ab10de2d5ba1bf2171be38e1e1c0e8fba
andrealfadila <anangandrealfadila@gmail.com>
2024-03-21 22:41:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-f8a2a3ab-4aba-4d32-a4b8-3a8842a71a64/)
 
 
 echo "$curl_output" >> Update.tsx
